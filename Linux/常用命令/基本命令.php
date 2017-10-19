736d2f4caaadc66f9806e5cfda157a67b4b0773d
yangyunhao <1126420614@qq.com>
2017-10-19 10:30:37 +0800

    Linux 基本命令

---
+++ Linux/常用命令/基本命令.php
@@ -0,0 +1,9 @@
+1、 ctrl + insert 复制 （选中文本）
+
+2、 shift + insert 粘贴 
+
+3、 cp 复制命令 如下
+
+    cp /data/www/basic/apixfb.xiaohe.com/lts/core/Config/database_shengchan.php ./database.php 
+
+       复制路径下的文件                                                          复制到哪里以及名字
